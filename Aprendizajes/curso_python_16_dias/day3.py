15a182d9f3e2ddbdecd82b2aae3ddccdcbf97f0f
Cesar Briones <cesarbrionesh2@gmail.com>
2025-02-24 09:17:07 -0600

    Add the practice projects of course 16days

---
+++ Aprendizajes/curso_python_16_dias/day3.py
@@ -0,0 +1,7 @@
+palabra = "ordenador"
+# print(palabra.index("r"))
+# frase = "Que pasa chavales, hoy estoy jugando con las mujeres"
+# print(frase.replace("jugando","mujeres","programando""Developers",))
+
+lista_palabras = ["La","legibilidad","cuenta."]
+print(f"{lista_palabras[0]} {lista_palabras[1]} {lista_palabras[2]}")
